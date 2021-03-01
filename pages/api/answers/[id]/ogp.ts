e5615a3615634c0dc75c58fca1fd552c14afddd9
motikoma <motikoma5125@gmail.com>
2021-03-01 23:38:21 +0900

    fix: 本番環境でnode-canvasが動くようにライブラリを追加

---
+++ pages/api/answers/[id]/ogp.ts
@@ -0,0 +1,86 @@
+import '../../../../lib/firebase_admin';
+import {firestore} from "firebase-admin";
+import { Answer, Question } from "../../../../interfaces/index";
+
+import * as path from "path";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createCanvas, registerFont } from 'canvas'
+
+
+// 文章の分割
+type SeparatedText = {
+  line: string,
+  remaining: string
+}
+
+const createTextLine = (context: any, text: string): SeparatedText => {
+  const maxWidth = 400;
+
+  for (let i = 0; i < text.length; i++){
+    const line = text.substring(0, i + 1);
+    if(context.measureText(line).width > maxWidth){
+      return {
+        line,
+        remaining: text.substring(i + 1)
+      }
+    }
+  }
+
+  return {
+    line: text,
+    remaining: ''
+  }
+}
+
+const createTextLines = (context: any, text: string): string[] => {
+  const lines: string[] = [];
+  let currentText = text;
+
+  while(currentText !== ''){
+    const separatedText = createTextLine(context, currentText);
+    lines.push(separatedText.line);
+    currentText = separatedText.remaining;
+  }
+
+  return lines;
+}
+// 文章の分割 -- ここまで
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+  const id = req.query.id as string;
+
+  const answerDoc = await firestore().collection("answers").doc(id).get();
+  const answer = await answerDoc.data() as Answer;
+
+  const questionDoc = await firestore().collection("questions").doc(answer.questionId).get();
+  const question = await questionDoc.data() as Question;
+
+  const width = 600;
+  const height = 315;
+  // path.resolveを使えばVersel上でも動かすことができる
+  registerFont(path.resolve("./fonts/ipaexg.ttf"),{
+      family: "ipaexg"
+  });
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext('2d');
+  context.fillStyle = '#fafafa';
+  context.fillRect(0, 0, width, height);
+  context.font = '20px ipagp';
+  context.fillStyle = '#424242';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  const lines = createTextLines(context, question.body)
+  lines.forEach((line, index) => {
+    const y = 157 + 40 * (index - (lines.length - 1) / 2)
+    context.fillText(line, 300, y)
+  })
+
+  const buffer = canvas.toBuffer();
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': buffer.length,
+  });
+  res.end(buffer, 'binary');
+}

b7da0e2a818ee2abb0fa51608faba007181b66d6
motikoma <motikoma5125@gmail.com>
2021-03-03 23:59:16 +0900

    fix: API検証

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -5,7 +5,12 @@ import { firestore } from 'firebase-admin'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const id = req.query.id as string
 
-  const doc = await firestore().collection('answers').doc(id).get()
+  // const doc = await firestore().collection('answers').doc(id).get()
 
-  res.status(200).json(doc.data())
+  const data = {
+    hoge: 1
+  }
+
+  // res.status(200).json(doc.data())
+  res.status(200).json(data);
 }

81b2a7ce428c0fd42738925e63792e2d17b808dc
motikoma <motikoma5125@gmail.com>
2021-03-04 22:38:05 +0900

    fix

---
+++ pages/api/hello.js
@@ -0,0 +1,20 @@
+import { NextApiResponse } from "next";
+// import '../../lib/firebase_admin'
+// import { firestore } from 'firebase-admin'
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const id = req.query.id as string
+
+//   const doc = await firestore().collection('answers').doc(id).get()
+
+//   const data = {
+//     hoge: 1,
+//   };
+
+//   res.status(200).json(doc.data())
+//   res.status(200).json(data);
+// };
+
+export default (req, res) => {
+  res.status(200).json({ text: 'Hello' })
+}

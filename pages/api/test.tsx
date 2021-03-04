6504db307403bb99387a8f52565b7535014ff1cc
motikoma <motikoma5125@gmail.com>
2021-03-04 22:15:12 +0900

    fix: test

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import '../../lib/firebase_admin'
-import { firestore } from 'firebase-admin'
+// import { firestore } from 'firebase-admin'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const id = req.query.id as string
+  // const id = req.query.id as string
 
   // const doc = await firestore().collection('answers').doc(id).get()
 

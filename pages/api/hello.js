065f9d104e1d964cc426147d0a4d29f6b30bdf34
motikoma <motikoma5125@gmail.com>
2021-03-08 21:53:53 +0900

    fix: firebase_adminとの接続確認

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,20 +1,11 @@
-import { NextApiResponse } from "next";
-// import '../../lib/firebase_admin'
-// import { firestore } from 'firebase-admin'
+import { NextApiRequest, NextApiResponse } from "next";
+import '../../lib/firebase_admin'
+import { firestore } from 'firebase-admin'
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const id = req.query.id as string
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id as string
 
-//   const doc = await firestore().collection('answers').doc(id).get()
+  const doc = await firestore().collection('answers').doc(id).get()
 
-//   const data = {
-//     hoge: 1,
-//   };
-
-//   res.status(200).json(doc.data())
-//   res.status(200).json(data);
-// };
-
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
-}
+  res.status(200).json(doc.data())
+};

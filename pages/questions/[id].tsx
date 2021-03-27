1ad2c5542b31dfc9092a6f152a2a242fc47e61fd
motikoma <motikoma5125@gmail.com>
2021-03-27 22:11:02 +0900

    fix: process.env.API_URL確認

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -19,6 +19,8 @@ export default function QuestionsShow() {
   const [body, setBody] = useState<string>("");
   const [question, setQuestion] = useState<Question | null>(null);
 
+  console.log(process.env.API_URL);
+
   async function loadData() {
     // --- 質問の読み込み ---
     if (query.id === undefined) {
